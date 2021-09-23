333fe79e5296375c77bc0e853cfe7a4a8118b442
GaianiMagali <magaliantonella.gaiani@gmail.com>
2021-09-23 11:04:41 -0300

    ImageSection.jsx

--- src/components/ImageSection.jsx
+++ src/components/ImageSection.jsx
@@ -10,13 +10,26 @@ export const ImageSection = () => {
 
             <div className="about-info">
                 <h4>I am<span> Lorem ipsum</span></h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae illo optio corporis enim. Deserunt quisquam sed eveniet obcaecati incidunt harum sequi laborum possimus iusto. Libero mollitia error ea vel illum.Bcaecati incidunt harum sequi laborum</p>
+                <p className="about-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae illo optio corporis enim. Deserunt quisquam sed eveniet obcaecati incidunt harum sequi laborum possimus iusto. Libero mollitia error ea vel illum.Bcaecati incidunt harum sequi laborum</p>
 
                 <div className="about-details">
-                    <div className="details">
-                        
+                    <div className="left-section">
+                        <p>Full Name</p>
+                        <p>Age</p>
+                        <p>Nationality</p>
+                        <p>Languages</p>
+                    </div>
+                    <div className="right-section">
+                        <p>: Magali Gaiani</p>
+                        <p>: 27</p>
+                        <p>: Argentina</p>
+                        <p>: Spanish</p>
                     </div>
                 </div>
+
+                <button className="btn">
+                    Download CV
+                </button>
             </div>
         </div>
     )
