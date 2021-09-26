fe2907b1d7516c3b777adf3ae771ec73dbd72557
GaianiMagali <magaliantonella.gaiani@gmail.com>
2021-09-26 04:53:36 -0300

    Blogs page

--- src/pages/BlogsPage.jsx
+++ src/pages/BlogsPage.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Title } from '../components/Title';
+import allBlogs from '../data/allBlogs';
 
 export const BlogsPage = () => {
     return (
-        <div>
-            <h1>BlogsPage</h1>
+        <div className="BlogsPage">
+            <div className="b-title">
+                <Title title={'Recent Blogs'} span={'Recent Blogs'} />
+            </div>
+
+            <div className="BlogsPage">
+                {
+                    allBlogs.map((blog, i) => {
+                        return <div className="blog" key={blog.id}>
+                            <div className="blog-content">
+                                <img src={blog.image} alt={blog.id + i} />
+                                <a href={blog.link} className="blog-link">{blog.title}</a>
+                                <div className="date-con">
+
+                                </div>
+                            </div>
+                        </div>
+                    })
+                }
+            </div>
         </div>
     )
 }
